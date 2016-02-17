f2585836827fbc65d6ad8dde85363c032e22f6fe
Jennifer Lu <julenn@gmail.com>
2016-02-16 19:46:55 -0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
-$(document).ready(function() {
-    console.log("Hello world.")
+$(document).ready(function(){
+    
+    getData();
 });
 
 
+function getData() {
+    $.getJSON("js/columbia.json", function(schoolsData) {
+    console.log(schoolsData);
+
+    loopThroughData(schoolsData);
+    })
+
+}
+
+function loopThroughData(s) {
+    
+    for (i=0; i<s.length; i++) {
+
+        var schoolName = s[i]["SCHOOL_NAME"];
+        var year = s[i]["YEAR"];
+        var gradPct = s[i]["GRADUATES_PCT"];
+
+        //if graduation percent is less than 50%...
+        if (gradPct < 50 && gradPct > 0) {
+        console.log(year, schoolName, gradPct);
+    }
+        
+
+            var barWidth = gradPct * 10;
+
+            if (schoolName === "FREDERICK DOUGLASS HIGH"){
+                $(".chart").append(
+                "<div class='bar' style='width: "+barWidth+"px'></div>");
+
+    }
 
 
-
-
+}
+}
